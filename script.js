2a50438653855620629f455f669ed1118fffbdba
herothezero90 <karl.a.lenard@gmail.co>
2024-09-29 16:37:14 +0200

    modal plus Elenco delle ultime 100 proprietà

---
+++ script.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+function resetForm() {
+  $("#modalForm")[0].reset();
+}
+
+$(document).ready(function () {
+  $("#clear-form").click(function () {
+    console.log("reset");
+    resetForm();
+  });
+});
